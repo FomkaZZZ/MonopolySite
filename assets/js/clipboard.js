367522b5537bcddef039e84f913477e22b816d82
FomkaZZZ <104300591+FomkaZZZ@users.noreply.github.com>
2025-07-08 17:45:03 +0500

    Initial commit

---
+++ assets/js/clipboard.js
@@ -0,0 +1,10 @@
+function copy(){
+    navigator.clipboard.writeText('mc.1poly.fun')
+    swal({
+    title: 'IP скопирован',
+    text: 'Приятной игры',
+    icon: 'success',
+    timer: 3000,
+    buttons: false,
+})
+}
